ca796d56e949e89adc1aa7748961719c7fac82d7
starg09 <ultraguille2006@gmail.com>
2019-12-20 00:39:03 -0300

    Update to the farming report v2 site

--- magireco/index.js
+++ magireco/index.js
@@ -1,9 +1,42 @@
-Survey.StylesManager.applyTheme("winterstone");
+Survey.StylesManager.applyTheme("bootstrap");
 
 var surveyJSON = { surveyId: 'cbc3a260-bcc4-4798-840a-79f23b6a4b5b'}
 
 function sendDataToServer(survey) {
-    survey.sendResult('8275e515-8f69-4dda-8199-d96a6470cf67');
+
+    var form_username = survey.data.username;
+    var farmed_node = survey.data.farmed_stage;
+    var farmed_times = survey.data.times_farmed;
+    var farmed_drops = survey.data.drops;
+
+    console.log("Username: " + form_username);
+    console.log("Farmed Node: " + farmed_node);
+    console.log("Farmed Times: " + farmed_times);
+    console.log("Dropped Items: " + farmed_drops);
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwFvU7Zu90dW_-sjFaQELAgFkXLP7iMy8Vj4OrVTZC84AmFFgA/exec';
+    
+    const form = document.createElement('form');
+    const form_params = {
+      'reportUser': form_username,
+      'nodeFarmed': farmed_node,
+      'timesFarmed': farmed_times,
+      'droppedItems': farmed_drops
+    };
+    for (const key in form_params) {
+      if (form_params.hasOwnProperty(key)) {
+        const hiddenField = document.createElement('input');
+        hiddenField.type = 'hidden';
+        hiddenField.name = key;
+        hiddenField.value = form_params[key];
+
+        form.appendChild(hiddenField);
+      }
+    }
+    
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
 }
 
 var survey = new Survey.Model(surveyJSON);
